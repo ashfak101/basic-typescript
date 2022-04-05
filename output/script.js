12d3ad078cd3cdf39349a33bd3fea7c448250cd2
Md Ashfakul Karim Rahi <ashfakulkarim2@gmail.com>
2022-04-05 15:46:32 +0600

    Types

--- output/script.js
+++ output/script.js
@@ -9,3 +9,23 @@ let playerName = 'AShraful';
 let age = 35;
 console.log(playerName);
 console.log(age);
+// function 
+function multiply(a, b) {
+    return a * b;
+}
+console.log(multiply(4, 5));
+// array
+let fruits = ['apple', 'banana', 'orange'];
+fruits.push("mango");
+console.log(fruits);
+let mixed = ["apple", 34, true];
+mixed.push(false);
+console.log(mixed);
+// object
+let person = {
+    name: 'Ashfakul',
+    age: 35,
+    location: true
+};
+person.age = 45;
+console.log(person);
