8805fa4d8bf9253c7aab434c715f107913582f64
Md Ashfakul Karim Rahi <ashfakulkarim2@gmail.com>
2022-04-05 15:03:42 +0600

    Initital commit

---
+++ output/script.js
@@ -0,0 +1,11 @@
+"use strict";
+const country = "bangladesh";
+console.log(country);
+const add = (num1, num2) => {
+    return num1 + num2;
+};
+console.log(add(2, 5));
+let playerName = 'AShraful';
+let age = 35;
+console.log(playerName);
+console.log(age);
---
+++ src/script.ts
@@ -0,0 +1,14 @@
+
+const country = "bangladesh";
+console.log(country)
+const add =(num1:number,num2:number):number=>{
+    return num1+num2
+}
+
+console.log(add(2,5));
+let playerName ='AShraful'
+let age = 35
+console.log(playerName);
+console.log(age)
+
+
